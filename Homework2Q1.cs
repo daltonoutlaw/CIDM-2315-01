3e5686387dca103ef89d47561e7de96a1f036929
daltonoutlaw <142956542+daltonoutlaw@users.noreply.github.com>
2023-09-05 23:10:02 -0500

    Add files via upload

---
+++ Homework2Q1.cs
@@ -0,0 +1,29 @@
+﻿namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please input a letter grade: ");
+        string letter = Console.ReadLine();
+        char letter_char = Convert.ToChar(letter);
+        if(letter_char == 'A'){
+            Console.WriteLine("GPA Point: 4");
+        }
+        else if (letter_char == 'B'){
+            Console.WriteLine("GPA Point: 3");
+        }
+        else if (letter_char == 'C'){
+            Console.WriteLine("GPA Point: 2");
+        }
+        else if (letter_char == 'D'){
+            Console.WriteLine("GPA Point: 1");
+        }
+        else if (letter_char == 'F'){
+            Console.WriteLine("GPA Point: 0");
+        }
+        else{
+            Console.WriteLine("Wrong Letter Grade!");
+        }
+    }
+}
