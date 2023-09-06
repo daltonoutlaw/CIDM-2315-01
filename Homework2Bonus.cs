15a5ea95add959676116fba732f93231597af319
daltonoutlaw <142956542+daltonoutlaw@users.noreply.github.com>
2023-09-06 00:37:22 -0500

    Add files via upload

---
+++ Homework2Bonus.cs
@@ -0,0 +1,27 @@
+﻿namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please input a year: ");
+        string year = Console.ReadLine();
+        int year_int = Convert.ToInt16(year);
+        if(year_int%100 == 0){
+            if(year_int%400 == 0){
+                Console.WriteLine(year_int+"is a leap year");
+            }
+            else{//year_int%100<=0
+                Console.WriteLine(year_int+"is not a leap year");
+            }
+        }
+        else{
+            if(year_int%4 == 0){
+                Console.WriteLine(year_int+"is a leap year");
+            }
+            else{//year_int%4<=0
+                Console.WriteLine(year_int+"is a not leap year");
+            }
+        }
+    }
+}
