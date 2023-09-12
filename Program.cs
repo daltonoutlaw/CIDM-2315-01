cee3d2aa6d7d5ebb57acb86f45ebe526ff745c29
daltonoutlaw <142956542+daltonoutlaw@users.noreply.github.com>
2023-09-12 14:28:53 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Week4Lecture;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = Convert.ToInt16(Console.ReadLine());
+        int b = Convert.ToInt16(Console.ReadLine());
+        Console.Write($"a = {a}; ");
+        Console.WriteLine($"b = {b}");
+        if(a>b){
+            Console.WriteLine($"The largest number is: {a}");
+        }
+        else{
+            Console.WriteLine($"The largest number is: {b}");
+        }
+    }
+}
