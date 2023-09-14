d9fbd890a31e89b09c84c287f7dd627dfa8c5f78
daltonoutlaw <142956542+daltonoutlaw@users.noreply.github.com>
2023-09-13 20:49:09 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Week4Lecture;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Assign an int value to N:");
+        int N = Convert.ToInt16(Console.ReadLine());
+        for(int row = 0; row<N; row++){
+            for(int col = 0; col<N; col++)
+            {
+                Console.Write('#');
+            }
+            Console.WriteLine("");
+        }
+
+    }
+}
