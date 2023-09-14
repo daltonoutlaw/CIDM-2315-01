ca9128b1dfe86ca9fc1fc50e51de19beea79ad0f
daltonoutlaw <142956542+daltonoutlaw@users.noreply.github.com>
2023-09-13 20:38:58 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Week4Lecture;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Input an integer: ");
+        int N = Convert.ToInt16(Console.ReadLine());
+        int i = 2;
+        while(N >= i)
+        {
+            if(N == 2){
+                Console.WriteLine("N is prime");
+                break;
+            }
+            else if(N%i == 0){
+                Console.WriteLine("N is non-prime");
+                break;
+            }
+            else if(N%i != 0){
+                Console.WriteLine("N is prime");
+                break;
+            }
+            i++;
+        }
+    }
+}
