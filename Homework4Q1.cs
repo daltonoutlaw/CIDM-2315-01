67fbbf8ffc08f8ff0e33f38f5f26786991004b48
daltonoutlaw <142956542+daltonoutlaw@users.noreply.github.com>
2023-09-19 19:55:43 -0500

    Update Homework4Q1.cs

--- Homework4Q1.cs
+++ Homework4Q1.cs
@@ -1,18 +1,24 @@
-﻿namespace Week4Lecture;
+namespace Week4Lecture;
 
 class Program
 {
     static void Main(string[] args)
     {
-        int a = Convert.ToInt16(Console.ReadLine());
-        int b = Convert.ToInt16(Console.ReadLine());
-        Console.Write($"a = {a}; ");
-        Console.WriteLine($"b = {b}");
+        Greatest();
+    }
+
+    static void Greatest()
+    {
+        int a=Convert.ToInt16(Console.ReadLine());
+        int b=Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine($"a = {a}; b = {b}");
         if(a>b){
-            Console.WriteLine($"The largest number is: {a}");
+            Console.WriteLine($"The largest num is: {a}");
         }
         else{
-            Console.WriteLine($"The largest number is: {b}");
+            Console.WriteLine($"The largest num is: {b}");
         }
     }
+
+
 }
