08b8a62d861388cc23f31b1245d59fed2daf699f
daltonoutlaw <142956542+daltonoutlaw@users.noreply.github.com>
2023-09-28 00:12:06 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Week4Lecture;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = Convert.ToInt16(Console.ReadLine());
+        int b = Convert.ToInt16(Console.ReadLine());
+        int c = Convert.ToInt16(Console.ReadLine());
+        int d = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine($"a = {a}; b = {b}; c = {c}; d = {d}");
+
+        Max(a,b,c,d);
+    }
+
+    static void Max(int a, int b, int c, int d)
+    {
+        if(a>b&&a>c&&a>d){
+            Console.WriteLine($"The largest number is: {a}");
+        }
+        else if(b>a&&b>c&&b>d){
+            Console.WriteLine($"The largest num is: {b}");
+        }
+        else if(c>a&&c>b&&c>d){
+            Console.WriteLine($"The largest num is: {c}");
+        }
+        else{
+            Console.WriteLine($"The largest num is: {d}");
+        }
+    }
+}
